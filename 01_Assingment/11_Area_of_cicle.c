68668678db19a02896e4cd8273ed6abee57b1fc0
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-23 16:28:46 +0530

    11_Area_of_cicle.c

---
+++ 01_Assingment/11_Area_of_cicle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main(){
+
+
+float Area = 0.0, Rad = 0.0;
+
+    printf("\n Enter the radius of cicle :");
+ scanf("%f",&Rad);
+
+    Area = 3.14* Rad *Rad;
+     printf("\n Area of cicle %0.2f = %0.2f",Rad,Area);
+
+     return 0;
+
+
+
+
+
+}
