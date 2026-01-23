964de993669c1e3e3b5d28eb74bcc626abf53f64
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-23 18:19:07 +0530

    12_Area_of_cirmference.c

---
+++ 01_Assingment/12_Area_of_cirmference.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+ int main(){
+
+ float Radius = 0.0, cirm = 0.0;
+
+     printf(" Enter radius of circle = ");
+     scanf("%f",&Radius);
+
+             cirm = 2* 3.14* Radius;
+
+             printf("calculate the circumference = %0.2f",cirm);
+
+             return 0;
+
+
+
+ }
