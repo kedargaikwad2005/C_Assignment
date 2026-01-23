c9bdaa5c431891d4720c25debd46400d691c47a3
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-23 18:19:45 +0530

    15_kilometer_to_meter.c

---
+++ 01_Assingment/15_kilometer_to_meter.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+ int main(){
+
+ float Kilometer = 0.0, Meter = 0.0;
+
+     printf(" Enter the Kilometer = ");
+     scanf("%f",&Kilometer);
+
+
+      Meter =  Kilometer * 1000;
+
+             printf("convert in into Meter = %0.2f ",Meter);
+
+             return 0;
+
+ }
