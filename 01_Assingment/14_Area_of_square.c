c16c23f625eeede862afb3229ddf6e71ad5b6173
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-23 16:35:20 +0530

    14_Area_of_square.c

---
+++ 01_Assingment/14_Area_of_square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+float side = 0.0, Area = 0.0;
+
+
+printf(" \Enter side length: ");
+scanf("%f",&side);
+
+
+        Area =  side * side;
+            printf("\n Area of square %0.2f = %0.2f ", side, Area);
+
+        return 0;
+
+}
