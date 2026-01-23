259bc7d82d2d2f0654f426f466b511ee7ad84eda
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-23 18:21:40 +0530

    17_square_of_number.c

---
+++ 01_Assingment/17_square_of_number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+
+     float no = 0.0, square = 0.0;
+
+     printf("Enter the number = ");
+     scanf("%f",&no);
+
+        square = no * no ;
+
+         printf(" calculates the square of given number = %0.2f", square);
+
+
+             return 0;
+
+ }
