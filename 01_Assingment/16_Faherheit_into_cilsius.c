b4be37712d0d7f501f92fb4fad7f81a9199b7b53
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-23 18:20:21 +0530

    16_Faherheit_into_cilsius.c

---
+++ 01_Assingment/16_Faherheit_into_cilsius.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+ int main(){
+
+ float Faherheit =0.0, cilsius =0.0;
+ printf("Enter the Faherheit =");
+ scanf("%f",&Faherheit);
+
+cilsius =(Faherheit - 32.0) * (5.0/9.0);
+
+
+  printf(" Faherheit into cilsius %0.2f ",cilsius);
+
+             return 0;
+
+
+ }
