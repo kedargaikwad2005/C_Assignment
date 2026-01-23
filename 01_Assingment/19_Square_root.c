1baa6d4ce23a9d8f462e3889ea9725e3b2d055e8
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-23 18:22:58 +0530

    19_Square_root.c

---
+++ 01_Assingment/19_Square_root.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+ int main()
+ {
+
+
+    float no = 0.0, squroot = 0.0;
+
+    printf(" Enter the number =");
+    scanf("%f",&no);
+
+    squroot = sqrt(no);
+
+    printf(" calculate the squar root of givn number = %0.2f",squroot);
+
+
+    return 0;
+ }
+
