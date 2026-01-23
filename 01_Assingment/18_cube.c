ea539b096a2e97238b2e5f998f8bf388cf3ad794
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-23 18:22:19 +0530

    18_cube.c

---
+++ 01_Assingment/18_cube.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+
+    float  no = 0.0, cube = 0.0;
+
+    printf("Enter the number =");
+    scanf("%f",&no);
+
+    cube = no * no * no;
+
+   printf("calculate the cube of given number = %0.2f", cube);
+
+             return 0;
+
+ }
