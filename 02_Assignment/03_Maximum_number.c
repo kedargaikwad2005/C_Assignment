4e1962738b8c2efb2a1417fbfd5ff8c30db9bf50
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-24 16:56:37 +0530

    03_Maximum_number.c

---
+++ 02_Assignment/03_Maximum_number.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+
+{
+    int N1, N2;
+
+    printf("\n Enter the 1st number");
+    scanf("%d",&N1);
+
+    printf("\n Enter the 2nd number");
+    scanf("%d",&N2);
+
+       if (N1 = N2)
+     {
+         printf("\n %d both number is same = ",N1,N2);
+     }
+
+
+
+    else if(N1 > N2)
+
+     {
+         printf("\n %d The 1st number is maximum = ",N1);
+     }
+
+
+
+     else
+     {
+         printf("\n %d The 2nd number is maximum = ",N2);
+     }
+
+
+
+
+
+
+    getch();
+    return 0 ;
+
+}
