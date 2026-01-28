30939f6ffed4f6b0a896adfd95a9dd9da0dbc463
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-01-28 15:31:43 +0530

    10_Print_hello_or_ world.c

---
+++ 02_Assignment/10_Print_hello_or_ world.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+
+{
+    int number = 0.0;
+    printf("\n Enter the number = ");
+    scanf("%d",&number);
+
+    if (number > 10 == 0)
+    {
+        printf("\n hello the number is less 10 ");
+    }
+    else
+    {
+        printf ("\n world the number is otherwise 10");
+    }
+
+
+    getch();
+    return 0 ;
+
+}
