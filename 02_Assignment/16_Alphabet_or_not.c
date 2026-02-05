07ec87eaef2d7dd881049e5b903a2a4c6c23dc92
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-02-05 16:14:03 +0530

    16_Alphabet_or_not.c

---
+++ 02_Assignment/16_Alphabet_or_not.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Ch = '\0';
+
+    printf("\n Enter Character To Check Upper or Lower Case  :");
+    scanf("%c",&Ch);
+
+    if( Ch >= 'A' && Ch <= 'Z' )
+    {
+        printf("\n Given Character %c is TRUE",Ch);
+    }
+    else if( Ch >= 'a' && Ch <= 'z' )
+    {
+       printf("\n Given Character %c is TRUE",Ch);
+    }
+    else
+    {
+        printf("\n The character %c is FALSE",Ch);
+    }
+    getch();
+    return 0;
+}
