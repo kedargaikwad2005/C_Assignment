ec404e60768fbd0afb855ff4b602235a16db4448
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-02-14 13:50:54 +0530

    02_loop_10_to_1.c

---
+++ 03_Assignment/02_loop_10_to_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i;
+
+      printf("\n Enter the number 10 to 1 =");
+
+      for( i = 10; i >=1; i--)
+        {
+            printf("\n %d",i);
+        }
+
+       getch();
+       return 0;
+
+
+
+}
