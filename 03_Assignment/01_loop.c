480ba678ac06c39e8c50d6577ceb4c50dbbd68a0
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-02-14 13:49:54 +0530

    01_loop.c

---
+++ 03_Assignment/01_loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i;
+
+      for( i = 101; i <= 110; i++)
+        {
+            printf("\n %d",i);
+        }
+
+       getch();
+       return 0;
+
+
+
+}
+
+      printf("\n Enter the number 101 to 110 =");
