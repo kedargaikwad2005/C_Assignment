f9ca2613919f05f43d6de92064b7e0f99aeeeacc
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-03-01 13:54:16 +0530

    02_Array Using Crate Initialization.c

---
+++ 05_Assignment/02_Array Using Charater Initalization.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+    int main()
+
+{
+    char ch [5] = {'X','M','A','N','G'};
+
+    printf("\n Array number : %c ",ch['0']);
+    printf("\n Array number : %c ",ch['1']);
+    printf("\n Array number : %c ",ch['2']);
+    printf("\n Array number : %c ",ch['3']);
+    printf("\n Array number : %c ",ch['4']);
+
+     getch();
+     return 0 ;
+
+}
