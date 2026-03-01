b314450baa995d4e7f4466a50fb6801c62378f07
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-03-01 13:52:50 +0530

    01_Array Without Initialization.c

---
+++ 05_Assignment/01_Array Without Initialization.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+    int main()
+
+{
+   int value [5] = {10,33,45,76,98,};
+
+    printf("\n Array number : %d ",value[0]);
+    printf("\n Array number : %d ",value[1]);
+    printf("\n Array number : %d ",value[2]);
+    printf("\n Array number : %d ",value[3]);
+    printf("\n Array number : %d ",value[4]);
+
+     getch();
+     return 0 ;
+
+}
