37914711a95719432386bb6100a750243985c73f
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-03-01 13:59:32 +0530

    03_Array of 7 elements with null initialization.c

---
+++ 05_Assignment/03_Array of 7elements with null initialization.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    int Arr[7] = {};
+
+    printf("\n number is %d",Arr[0]);
+
+    printf("\n number is %d",Arr[1]);
+
+    printf("\n number is %d",Arr[2]);
+
+    printf("\n number is %d",Arr[3]);
+
+    printf("\n number is %d",Arr[4]);
+
+    printf("\n number is %d",Arr[5]);
+
+    printf("\n number is %d",Arr[6]);
+
+    getch();
+    return 0;
+}
