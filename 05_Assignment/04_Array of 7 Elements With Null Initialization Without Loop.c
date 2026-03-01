08d84c259b23d332cdf6303c7eb8cb7d72381dd1
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-03-01 14:01:45 +0530

    04_Array of 7 Elements With Null Initialization Without Loop.

---
+++ 05_Assignment/04_Array of 7 Elements With Null Initialization Without Loop.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+   int Arr[7] = {};
+
+   printf("Enter value of Element  :");
+   scanf("%d",&Arr[0]);
+   printf("Enter value of Element  :");
+   scanf("%d",&Arr[1]);
+   printf("Enter value of Element  :");
+   scanf("%d",&Arr[2]);
+   printf("Enter value of Element  :");
+   scanf("%d",&Arr[3]);
+   printf("Enter value of Element  :");
+   scanf("%d",&Arr[4]);
+   printf("Enter value of Element  :");
+   scanf("%d",&Arr[5]);
+   printf("Enter value of Element  :");
+   scanf("%d",&Arr[6]);
+   printf("Enter value of Element  :");
+   scanf("%d",&Arr[7]);
+
+   getch();
+   printf("\n<=================******===================> \n");
+
+   printf("\n The value of Element is = %d",Arr[0]);
+   printf("\n The value of Element is = %d",Arr[1]);
+   printf("\n The value of Element is = %d",Arr[2]);
+   printf("\n The value of Element is = %d",Arr[3]);
+   printf("\n The value of Element is = %d",Arr[4]);
+   printf("\n The value of Element is = %d",Arr[5]);
+   printf("\n The value of Element is = %d",Arr[6]);
+   printf("\n The value of Element is = %d",Arr[7]);
+
+
+    getch();
+    return 0;
+}
