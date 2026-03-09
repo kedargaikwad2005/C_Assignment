e2cb146ed00c4de01237000a3c6a41c1a2814bfc
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-03-09 15:32:40 +0530

    11_Array Find Count Of Null Elements.c

---
+++ 05_Assignment/11_Array Find Count Of Null Elements.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int Bill[10] = {},i = 0 , Ecnt = 0;
+
+   printf("\n Enter All Bill Number To Check Count Old Number \n");
+
+   for( i = 0; i < 10; i++)
+   {
+       printf("\n Enter Bill No %d is : ",i+1);
+       scanf("%d",&Bill[i]);
+
+       if( Bill [i] == 0)
+       {
+           Ecnt++;
+       }
+   }
+   printf("\n\n The Count of Old Number is %d",Ecnt);
+   getch();
+   return 0;
+}
