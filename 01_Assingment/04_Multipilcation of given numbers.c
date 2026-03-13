d56feb49e3d80fb020323717289f016e300c49e7
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-03-13 15:16:14 +0530

    Add files via upload

---
+++ 01_Assingment/04_Multipilcation of given numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+int N1,N2,Res;
+
+        printf( "Enter the first number:");
+    canf("%d",&N1);
+
+        printf("Enter the second number:");
+    scanf("%d",&N2);
+
+    Res = N1 * N2;
+
+printf("Multiplication of two number = %d",Res);
+
+return 0;
+
+}
+
