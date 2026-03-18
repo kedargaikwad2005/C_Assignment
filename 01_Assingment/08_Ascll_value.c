a39f08a4f178231f333d66a9cd30e0459905ad66
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-03-18 11:50:07 +0530

    08_Ascll_value.c

---
+++ 01_Assingment/08_Ascll_value.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+    int main()
+{
+    int ch = '\0';
+
+    printf("\n Alphbeta Ascll table  =>");
+    scanf("%d",&ch);
+
+    for (ch = 0 ; ch <= 127 ; ch++)
+    {
+        printf("%d \t %c\n",ch, ch);
+    }
+        printf("\n\n Thanks of  visiting");
+
+    getch();
+    return 0;
+}
