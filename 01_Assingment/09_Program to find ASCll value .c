ef0b5d1b1ecc937d2d6d4e515febd9c2838cc890
kedargaikwad2005 <gaikwadkedar69@gmail.com>
2026-03-18 11:54:18 +0530

    09_Program to find ASCll value .c

---
+++ 01_Assingment/09_Program to find ASCll value .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Ascll;
+    char ch = '\0';
+
+    printf("\n Enter the value ");
+    scanf("%d",&Ascll);
+
+    ch = Ascll;
+    printf("\n character of Ascll %d = %c",Ascll ,ch);
+
+     printf("\n\n Thanks of  visiting");
+
+     getch();
+     return 0;
+
+}
+
